2a3331e5f16dd73b8fb4b8f79ccf5324407f2710
Grant <grantfact@gmail.com>
2023-11-12 23:44:49 -0500

    Fixed Order N Isomorphisms (almost correct)

--- CTCE Test.py
+++ CTCE Test.py
@@ -2,6 +2,7 @@ import sympy as smp
 import numpy as np
 import sys
 import pynauty as pyn
+import networkx as nx
 
 
 
@@ -16,6 +17,17 @@ C = smp.symbols('C')
 def expand(N,Order_C): 
     
          
+    """
+    Expands the Meyer expansion symbolically and converts from sympy objects to a list.
+
+    Parameters:
+    - N: int, the given order N of interest
+    - Order_C: int, the order of C
+
+    Returns:
+    - list: List of terms in the expansion
+    """
+         
     expression = 0
     A = []
     Collected_terms = []
@@ -26,8 +38,9 @@ def expand(N,Order_C):
     expr = smp.expand(smp.prod(A)-1) #multiply each term and subtract unity
     collected = smp.collect(expr,C) #Collect the expansion in orders of C
 
-    print("collected == ", collected)
+    # print("collected == ", collected)
 
+    global N1
     N1 = smp.binomial(N,2)
     
     for i in range(N1,0,-1): #This is just a weird quirk of how Sympy add objects are stored. 
@@ -53,84 +66,252 @@ def expand(N,Order_C):
         print('Order_C must be an Integer')
 
 
-#Takes termlist and converts to a dictionary with keys for each order of C with order 1 at the 0 index. 
-def IsConnected (termlist,N):
-    elementdict = dict.fromkeys(range(0,N)) #Dictionary of graphs with key of the order of C
-    for i, term in enumerate(termlist):
-        B = [] #toy list
-        if i == len(termlist)-1:
-            new_tuple = tuple()
-            for i, element in enumerate(term.args):
-                new_tuple += element.indices
-            B.append(new_tuple) #add it to list 
-        else:
-            for argi in term.args:
-                if type(argi) == smp.tensor.indexed.Indexed: #if term is a solo term (Order C = 1)
-                    B.append(tuple(smp.sets.sets.FiniteSet(*list(argi.indices))) )
-                if type(argi) == smp.core.mul.Mul: #if term is a multiplication of terms (Order C != 1)
-                    new_list = argi.args
-                    itemset = smp.sets.sets.FiniteSet(*list(new_list[0].indices))
-                    new_tuple = tuple()
-                    subset = smp.sets.sets.FiniteSet(*list(new_list[0].indices))
-                    for i, argi2 in enumerate(new_list): #parse through each F in the multiplication
-                        
-                        if i== 0: #initialize set for first term
-                            new_tuple += tuple(argi2.indices)
-                        if i >= 1: 
-                            intersection = itemset.intersect(subset) #calculate intersection 
-                            if len(intersection) > 0:
-                                new_tuple += tuple(argi2.indices) #save the connected indices
-                                subset.union(intersection) #make the set include the new index
-                    B.append(new_tuple) #add it list 
+def IsConnected_old (termlist,N):
+    
+    
+    if N ==2: 
+        B = []
+        for i, term in enumerate(termlist):
+            B.append(term.indices)
+        elementdict = {0:[B]}
+    else:
+        elementdict = dict.fromkeys(range(0,N1)) #Dictionary of graphs with key of the order of C
+        for i, term in enumerate(termlist):
+            B = [] #toy list to be appended later
 
-        elementdict[i] = B #then add to dict
+            if i == len(termlist)-1:
+                new_tuple = tuple()
+                for i, element in enumerate(term.args):
+                    new_tuple += element.indices
+                B.append(new_tuple) #add it to list 
+            else:
+                for argi in term.args:
+                    is_connected = True
+                    if type(argi) == smp.tensor.indexed.Indexed: #if term is a solo term (Order C = 1)
+                        B.append(tuple(smp.sets.sets.FiniteSet(*list(argi.indices))) )
+                    if type(argi) == smp.core.mul.Mul: #if term is a multiplication of terms (Order C != 1)
+                        new_list = argi.args
+                        itemset = smp.sets.sets.FiniteSet(*list(new_list[0].indices))
+                        new_tuple = tuple()
+                        subset = smp.sets.sets.FiniteSet(*list(new_list[0].indices))
+                        for i, argi2 in enumerate(new_list): #parse through each F in the multiplication
+                            
+                            if i== 0: #initialize set for first term
+                                new_tuple += tuple(argi2.indices)
+                            if i >= 1 and is_connected == True: 
+                                itemset = smp.sets.sets.FiniteSet(*list(argi2.indices))
+                                intersection = itemset.intersect(subset) #calculate intersection 
+                                if len(intersection) > 0:
+                                    new_tuple += tuple(argi2.indices) #save the connected indices
+                                    subset.union(intersection) #make the set include the new index
+                                if len(intersection) == 0:
+                                    is_connected = False
+                                    break
+                                
+                        if is_connected == True:
+                            B.append(new_tuple) #add it list 
+                        else:
+                            continue
+
+            if is_connected == True:
+                elementdict[i] = B #then add to dict
+            else:
+                continue
                         
                         
 
     return elementdict # Rtur
 
+def Makegraph(edgelist):
+    G = nx.Graph()
+    for i in range(len(edgelist)):
+        G.add_edge(edgelist[i][0],edgelist[i][1])
+    return G
 
-
-# Calculate Isomorphism of all graphs in dict and keep tally of how many of each graph show up in a list
-def isomorphism(dictionary,N):
-    graph_dict = {} #New form of graphs as a list of tuples with first element being the adjacency list of graph
-                    # and second being the number of times that graph shows up
-    for key,value in dictionary.items(): 
-        graph_dict[key] = []
-        for graph in value:
-            Adjacency_list = {key:[] for key in range(0,N)}#make dict of empty lists
-            for i in range(len(graph)): #appending each tuple F index to an adjacency list
-                if i%2 ==0:
-                    Adjacency_list[graph[i]-1].append(graph[i+1]-1)
-                if i%2 ==1:
-                    Adjacency_list[graph[i]-1].append(graph[i-1]-1)
-            graph_dict[key].append((Adjacency_list,1))
+def IsConnected (termlist,N):
     
-    for key,value in graph_dict.items():
-        for i in range(0,len(value)):
-            for j in range(i+1,len(value)):
-                print(value[i][0],value[j][0])
-                Graph1 = pyn.Graph(number_of_vertices=N,directed=False,adjacency_dict=value[i][0],vertex_coloring=[set(range(0,N))])
-                Graph2 = pyn.Graph(number_of_vertices=N,directed=False,adjacency_dict=value[j][0],vertex_coloring=[set(range(0,N))])
-                if pyn.isomorphic(Graph1,Graph2) == True:
-                    value.remove(value[j])
-                    value[i][1] += 1
+    
+    if N ==2: 
+        B = []
+        for i, term in enumerate(termlist):
+            B.append(term.indices)
+        elementdict = {0:[B]}
+    else:
+        elementdict ={new_list:[] for new_list in range(0,N1)} #Dictionary of graphs with key of the order of C
+        for i, term in enumerate(termlist):
+            B = [] #toy list to be appended later
+
+            if i == len(termlist)-1:
+                new_tuple = tuple()
+                for j, element in enumerate(term.args):
+                    new_tuple += element.indices
+                elementdict[i].append([new_tuple]) #add it to list 
             else:
-                continue
+                for argi in term.args:
+                    B = []
+
+                    if type(argi) == smp.tensor.indexed.Indexed: #if term is a solo term (Order C = 1)
+
+                        B.append(argi.indices)
+                    if type(argi) == smp.core.mul.Mul: #if term is a multiplication of terms (Order C != 1)
+                        new_list = argi.args
+
+                        for k, argi2 in enumerate(new_list): #parse through each F in the multiplication
+
+                            B.append(argi2.indices)
+                                
+
+                    Graph = Makegraph(B)
+                    if nx.is_connected(Graph) == True:
+                        elementdict[i].append(B)
+                                
+                            
+    return elementdict # Rtur
+
+def custom_equiv_set(lst, equivalence_relation):
+    
+    """
+    Finds unique elements in a list based on a custom equivalence relation.
+
+    Parameters:
+    - lst: list, input list
+    - equivalence_relation: function, custom equivalence relation
+
+    Returns:
+    - list: List of unique elements
+    - list: List of counts for each unique element
+    """
+    
+    counting_list = []
+    a=0
+    result = []
+    for i,item in enumerate(lst):
+        
+        if i == 0: 
+            counting_list.append([item,1])
+            result.append(item)
+            continue
+        
+        equivalent_found = False
+
+        for j,seen_item in enumerate(result):
+            if equivalence_relation(item, seen_item)==True:
+                equivalent_found = True
+                # a=j
+                # seen_counter+=1
+                counting_list[j][1] += 1
+                break
+        
+        if not equivalent_found:
+            result.append(item)
+            counting_list.append([item,1])
             
-    return graph_dict
+        # counting_list[a][1] += seen_counter
+
+    return result, counting_list
 
 
-def main(N,Order_C):
+def MakeAdjlist(Graph,N):
+    
+    """
+    Makes an adjacency list from a list of tuples.
+
+    Parameters:
+    - graph: list, list of tuples representing edges
+    - N: int, the number of vertices
+
+    Returns:
+    - dict: Adjacency list
+    """
+    
+    Adjacency_list = {key:[] for key in range(0,N)}#make dict of empty lists
+    for edge in Graph:
+        Adjacency_list[edge[0]-1].append(edge[1]-1)
+        Adjacency_list[edge[1]-1].append(edge[0]-1)
+    return Adjacency_list
+
+
+def isomorphism(dictionary,N):
+    
+    """
+    Calculates isomorphism of all graphs in the dictionary and keeps tally of each graph's occurrences.
+
+    Parameters:
+    - dictionary: dict, dictionary of graphs with keys as the order of C
+    - N: int, the number of vertices
+
+    Returns:
+    - dict: Dictionary of isomorphic graphs for each order of C
+    - dict: Condensed dictionary with the tally of each graph
+    """
+    
+    graph_dict = {} #New form of graphs as a dictionary of list of graph objects for each order of C
+    if  N ==2:
+        iso_dict = {0:[[pyn.Graph(number_of_vertices=2,directed=False,adjacency_dict= {0:[1],1:[0]},vertex_coloring=[set([0,1])])],1]}
+        condensed_dict = {0:[1]}
+    
+    else:
+        for key,value in dictionary.items(): 
+            graph_dict[key] = []
+            for graph in value:
+              
+                Adj_list = MakeAdjlist(graph,N)
+                #turn this adjacency list into graph objects
+                new_graph = pyn.Graph(number_of_vertices=N,adjacency_dict=Adj_list,directed=False,vertex_coloring=[set(range(0,N))])
+                graph_dict[key].append(new_graph)
+    
+    #new dictionary of isomorphic graphs for each order of C
+        iso_dict ={} 
+        condensed_dict = dict.fromkeys(range(1,N1+1))
+        for key,value in graph_dict.items():
+            new_value2 = []
+            if len(value) > 1:
+                new_value = custom_equiv_set(value,pyn.isomorphic)[1]
+                iso_dict[key] = new_value
+                for item in new_value:
+                    new_value2.append(item[1])
+                condensed_dict[key+1] = new_value2
+                
+            if len(value) == 1:
+                new_value = [value,1]
+                iso_dict[key] = new_value
+                condensed_dict[key+1] = [1]
+            
+            
+    return iso_dict ,condensed_dict
+
+
+def main(N,Order_C,print_bool):
+    
+    """
+    Main function to execute the entire process.
+
+    Parameters:
+    - N: int, the given order N of interest
+    - Order_C: int, the order of C
+    - print_bool: bool, whether to print intermediate results or not
+
+    Returns:
+    None
+    """
+    
     Collected = expand(N,Order_C)
-    print("expansion == ", Collected)
     Full_list = IsConnected(Collected,N)
-    print("Connected Terms == ", Full_list)
-    unique_list = isomorphism(Full_list,N)
-    print("Unique List == ", unique_list)
+    unique_list = isomorphism(Full_list,N)[0]
+    condensed_dict = isomorphism(Full_list,N)[1]
+    
+    
+    
+    if print_bool == True:
+        print("Connected Terms == ", Full_list)
+        # print("expansion == ", Collected)
+        print("Unique List == ", unique_list)
+    
+    print("Condensed List == ", condensed_dict)
+    
     return
 
 if __name__ == "__main__":
-    main(3,0)
+    main(4,0,True)
 
 
--- test.py
+++ test.py
@@ -1,11 +1,55 @@
 import pynauty as pyn
 
-A = pyn.Graph(number_of_vertices=3,directed=False,adjacency_dict={0:[1],1:[0],2:[]},vertex_coloring=[set([0,1]),set([2])])
-B = pyn.Graph(number_of_vertices=3,directed=False,adjacency_dict={0:[2],1:[],2:[0]},vertex_coloring=[set([0,1]),set([2])])
+A = pyn.Graph(number_of_vertices=3,directed=False,adjacency_dict={0:[1],1:[0],2:[]},vertex_coloring=[set([0,1,2])])
+B = pyn.Graph(number_of_vertices=3,directed=False,adjacency_dict={0:[2],1:[],2:[0]},vertex_coloring=[set([0,1,2])])
+C = pyn.Graph(number_of_vertices=3,directed=False,adjacency_dict={0:[2,1],1:[0],2:[0]},vertex_coloring=[set([0,1,2])])
+D = pyn.Graph(number_of_vertices=4,directed=False,adjacency_dict={0:[2,1],1:[3,0],2:[0],3:[1]},vertex_coloring=[set([0,1,2,3])])
 
-C = pyn.Graph({0:[1],1:[0],2:[]})
-D = pyn.Graph({0:[2],1:[],2:[0]})
+E = pyn.Graph(number_of_vertices=4,directed=False,adjacency_dict={0:[1],1:[0],2:[3,0],3:[2,1]},vertex_coloring=[set([0,1,2,3])])
+F = pyn.Graph(number_of_vertices=4,directed=False,adjacency_dict={0:[1],1:[2,0],2:[1,3],3:[2]},vertex_coloring=[set([0,1,2,3])])
+list = [A,B,C,D,E,F]
+
+def custom_equiv_set(lst, equivalence_relation):
+    
+    seen = []
+    counting_list = []
+
+    for i,item in enumerate(lst):
+        seen_counter = 0
+        if i == 0: 
+            counting_list.append([item,1])
+            seen.append(item)
+            continue
+        
+        equivalent_found = False
+
+        for j,seen_item in enumerate(seen):
+            if equivalence_relation(item, seen_item)==True:
+                equivalent_found = True
+                a=j
+                seen_counter+=1
+                break
+        
+        if not equivalent_found:
+            seen.append(item)
+            counting_list.append([item,1])
+            
+        counting_list[a][1] += seen_counter
 
 
-print(pyn.isomorphic(C,D))
-print(pyn.isomorphic(A,B))
+    return seen, counting_list
+
+# Example usage:
+# Define your custom equivalence relation function, for example:
+def custom_equiv(a, b):
+    # Your equivalence logic here
+    return pyn.isomorphic(a,b)   # Example: consider two numbers equivalent if they have the same parity
+
+# Test the algorithm
+input_list = [A,B,C,D,E,F]
+result_set, element_counter = custom_equiv_set(input_list, custom_equiv)
+# print("Result Set:", result_set)
+print("Element Counter:", element_counter)
+
+
+
